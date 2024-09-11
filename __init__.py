ce1ed3aac8ed8cd8719b2be6ae613cec4ed2cfe1
ClaraNO <71319959+ClaraNO@users.noreply.github.com>
2024-09-11 09:43:43 +0200

    Update __init__.pyHistogramme

--- __init__.py
+++ __init__.py
@@ -31,9 +31,10 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
+# Route pour afficher l'histogramme
 @app.route("/histogramme/")
-def mongraphique():
-    return render_template("histogramme.html")
+def histogramme():
+    return render_template("histogramme.html")
 
   
 if __name__ == "__main__":
